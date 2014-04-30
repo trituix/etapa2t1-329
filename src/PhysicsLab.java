acf38a0fa5d06c26f562a1008b75d079904e4281
Gonzalo Sanchez <johndoe@example.com>
2014-04-30 18:11:16 -0400

    Etapa 2 funcionando

--- src/PhysicsLab.java
+++ src/PhysicsLab.java
@@ -13,7 +13,7 @@ public class PhysicsLab {
       double radius = 0.1;    // 10 [cm] 
       double position = 0;  // 0 [m] 
       double speed = 0;     // 0 [m/s]
-      Ball b0 = new Ball(mass, radius, -1.5, speed);
+      Ball b0 = new Ball(mass, radius, 0, speed);
       Ball b1 = new Ball(mass, radius, 1.5, 0);
       Spring spring = new Spring(1.0, 1.0);
       spring.attachEnd(b0);
